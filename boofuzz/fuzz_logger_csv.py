bc2c1e91c27bfb012a209bd036db512174ad5b1a
Matteo Rosi <mrosi@omniagroup.it>
2017-06-23 15:58:04 +0200

    Clean code

--- boofuzz/fuzz_logger_csv.py
+++ boofuzz/fuzz_logger_csv.py
@@ -4,7 +4,6 @@ import sys
 import time
 import datetime
 import csv
-from colorama import Fore, Back, Style, init
 
 from . import helpers
 from . import ifuzz_logger_backend
@@ -42,17 +41,6 @@ class FuzzLoggerCsv(ifuzz_logger_backend.IFuzzLoggerBackend):
     This class formats FuzzLogger data for pcap file. It can be
     configured to output to a named file.
     """
-    TEST_CASE_FORMAT = Fore.YELLOW + Style.BRIGHT + "Test Case: {0}" + Style.RESET_ALL
-    TEST_STEP_FORMAT = Fore.MAGENTA + Style.BRIGHT + "Test Step: {0}" + Style.RESET_ALL
-    LOG_ERROR_FORMAT = Back.RED + Style.BRIGHT + "Error!!!! {0}" + Style.RESET_ALL
-    LOG_CHECK_FORMAT = "Check: {0}"
-    LOG_INFO_FORMAT = "Info: {0}"
-    LOG_PASS_FORMAT = Fore.GREEN + Style.BRIGHT + "Check OK: {0}" + Style.RESET_ALL
-    LOG_FAIL_FORMAT = Fore.RED + Style.BRIGHT + "Check Failed: {0}" + Style.RESET_ALL
-    LOG_RECV_FORMAT = Fore.CYAN + "Received: {0}" + Style.RESET_ALL
-    LOG_SEND_FORMAT = Fore.CYAN + "Transmitting {0} bytes: {1}" + Style.RESET_ALL
-    DEFAULT_TEST_CASE_ID = "DefaultTestCase"
-    INDENT_SIZE = 2
 
     def __init__(self, file_handle=sys.stdout, bytes_to_str=DEFAULT_HEX_TO_STR):
         """
@@ -68,62 +56,31 @@ class FuzzLoggerCsv(ifuzz_logger_backend.IFuzzLoggerBackend):
 
     def open_test_step(self, description):
         None
-        # self._print_log_msg(self.TEST_STEP_FORMAT.format(description),
-        #                     indent_level=1)
 
     def log_check(self, description):
         None
-        # self._print_log_msg(self.LOG_CHECK_FORMAT.format(description),
-        #                     indent_level=2)
 
     def log_error(self, description):
         None
-        # self._print_log_msg(self.LOG_ERROR_FORMAT.format(description),
-        #                     indent_level=2)
 
     def log_recv(self, data):
         self._print_log_msg(["recv", self._format_raw_bytes(data),data])
-        # self._print_log_msg(self.LOG_RECV_FORMAT.format(self._format_raw_bytes(data)),
-        #                     indent_level=2)
 
     def log_send(self, data):
         self._print_log_msg(["send", self._format_raw_bytes(data),data])
-        # self._print_log_msg(
-        #     self.LOG_SEND_FORMAT.format(len(data), self._format_raw_bytes(data)),
-        #     indent_level=2)
 
     def log_info(self, description):
         None
-        # self._print_log_msg(self.LOG_INFO_FORMAT.format(description),
-        #                     indent_level=2)
 
     def open_test_case(self, test_case_id):
-        None
-        # self._print_log_msg(self.TEST_CASE_FORMAT.format(test_case_id),
-        #                     indent_level=0)
+        self._print_log_msg(["", "", ""])
 
     def log_fail(self, description=""):
         None
-        # self._print_log_msg(self.LOG_FAIL_FORMAT.format(description),
-        #                     indent_level=3)
 
     def log_pass(self, description=""):
         None
-        # self._print_log_msg(self.LOG_PASS_FORMAT.format(description),
-        #                     indent_level=3)
 
     def _print_log_msg(self, msg, indent_level=0):
-        # msg = _indent_all_lines(msg, indent_level * self.INDENT_SIZE)
         time_stamp = get_time_stamp()
         self._csv_handle.writerow([time_stamp] + msg)
-        # print(time_stamp + ' ' + _indent_after_first_line(msg, len(time_stamp) + 1), file=self._file_handle)
-
-
-def _indent_all_lines(lines, amount, ch=' '):
-    padding = amount * ch
-    return padding + ('\n' + padding).join(lines.split('\n'))
-
-
-def _indent_after_first_line(lines, amount, ch=' '):
-    padding = amount * ch
-    return ('\n' + padding).join(lines.split('\n'))
